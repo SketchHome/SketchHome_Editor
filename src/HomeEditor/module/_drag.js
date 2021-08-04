bebf77e82505be259ad48cfd707f155c1ba8fbea
ire4564 <ire4564@gmail.com>
2021-08-04 13:26:47 +0900

    Update drag module

--- src/HomeEditor/module/_drag.js
+++ src/HomeEditor/module/_drag.js
@@ -56,6 +56,9 @@ export const relocateDragTarget = (target, view_mode) => {
         else if (target.name.split("_")[0] === "window") {
             relocateWindow_3D(target);
         }
+        else if (target.name.split("_")[0] === "door") {
+            relocateDoor_3D(target);
+        }
     }
 }
 
@@ -130,27 +133,78 @@ const relocateDoor_2D = (target) => {
 const relocateWindow_3D = (target) => {
     target.parent.children.forEach(obj => {
         if (obj.name.split("_")[0] === "wall") {
+
+            console.log("target name:", target.name);
+            console.log("target parent name :", target.parent.name);
+            console.log("obj name:", obj.name);
+            console.log("obj parent name :", obj.parent.name);
+            console.log("obj wall_type :", obj.wall_type);
+
             // fixed x, z axis
             switch (obj.wall_type) {
                 case "horizon":
-                    target.position.z = obj.position.z;
-                    target.position.x = target.window_position.x;
+                    target.position.z = obj.position.z; //fixed Z axis
+
+                    // move X axis
+                    const minX = -((obj.scale.x/2) - (target.scale.x/2));
+                    const maxX = ((obj.scale.x/2) - (target.scale.x/2));
+                    if (minX < target.position.x && target.position.x < maxX)
+                        target.window_position.x = target.position.x;
+                    else 
+                        target.position.x = (target.position.x > minX) ? maxX : minX;
                     break;
                 case "vertical":
-                    target.position.x = obj.position.x;
-                    target.position.z = target.window_position.z;
+                    target.position.x = obj.position.x; //fixed X axis
+
+                    // move Z axis
+                    const minZ = -((obj.scale.z/2) - (target.scale.x/2));
+                    const maxZ = ((obj.scale.z/2) - (target.scale.x/2));
+                    if (minZ < target.position.z && target.position.z < maxZ)
+                        target.window_position.z = target.position.z;
+                    else 
+                        target.position.z = (target.position.z > minZ) ? maxZ : minZ;
                     break;
                 default:
                     break;
             }
 
-            // move z axis
-            const min = target.window_size.y/2;
-            const max = (obj.scale.y - target.window_size.y/2);
-            if (min < target.position.y && target.position.y < max)
+            // move Y axis
+            const minY = target.window_size.y/2;
+            const maxY = (obj.scale.y - target.window_size.y/2);
+            if (minY < target.position.y && target.position.y < maxY)
                 target.window_position.y = target.position.y;
             else 
-                target.position.y = (target.position.y > min) ? max : min;
+                target.position.y = (target.position.y > minY) ? maxY : minY;
         }
     });
+}
+
+const relocateDoor_3D = (target) => {
+    target.parent.children.forEach((obj) => { // target : door, obj : wall
+        if (obj.name.split("_")[0] === "wall") {
+            switch (obj.wall_type) {
+                case "horizon" :
+                    target.position.z = obj.position.z; // fix position
+                    //target.door_position.x = obj.position.x;
+                    const horizonMin = -1 * ((obj.scale.x / 2) - (target.scale.x / 2));
+                    const horizonMax = ((obj.scale.x / 2) - (target.scale.x / 2));
+                    if (target.position.x < horizonMin) target.position.x = horizonMin;                    
+                    else if (target.position.x > horizonMax) target.position.x = horizonMax;
+                    break;
+                case "vertical" :
+                    target.position.x = obj.position.x; // fix position
+                    //target.door_position.z = obj.position.z;
+                    const verticalMin = -1 * ((obj.scale.z / 2) - (target.scale.x / 2))
+                    const verticalMax = ((obj.scale.z / 2) - (target.scale.x / 2));
+                    if (target.position.z < verticalMin) target.position.z = verticalMin;                    
+                    else if (target.position.z > verticalMax) target.position.z = verticalMax;
+                    break;
+                default :
+                    break;
+            }
+            target.position.y = target.scale.y / 2;
+
+
+        }
+    })
 }
