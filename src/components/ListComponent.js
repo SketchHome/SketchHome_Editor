557b6b8c62144e747c8f65ffa9560447c848a541
ire4564 <ire4564@gmail.com>
2021-08-18 18:02:16 +0900

    Modify Side bar component-check box

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 
+//image
 import wood3 from '../resource/image/wood3.png';
+import btnImage from '../resource/image/checkNone.png';
+import btnCheck from '../resource/image/checkThis.png';
 
 function ListComponent(props) {
     //radio button
     const [ inputStatus, setInputStatus ] = useState(false);
+    const [ btnImages, setImage ] = useState(btnImage);
+
     const handleClickRadioButton = (radioBtnName) => {
+        setImage(btnCheck);
         setInputStatus(radioBtnName)
+        console.log(inputStatus)
     }
 
     return (
@@ -15,11 +22,12 @@ function ListComponent(props) {
                     <input 
                         type= 'radio'
                         id= {props.id} 
-                        value= 'wood1'
+                        value= {props.value}
                         name = "woods"
-                        checked={inputStatus === 'wood1'}
-                        onClick={() => handleClickRadioButton('wood1')}
+                        checked={inputStatus === props.value}
+                        onClick={() => handleClickRadioButton(props.value)}
                         className="radioTrue"
+                        style={{backgroundImage: inputStatus === props.value ? `url(${btnCheck})` : `url(${btnImage})`, backgroundSize: "35px 35px"}}
                     />
                     <label htmlFor='wood1'></label>
             </div>
