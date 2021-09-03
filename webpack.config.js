ea961574ad8b14f781d0ffcb960f07e6bcb5b9b2
ire4564 <ire4564@gmail.com>
2021-09-03 18:02:39 +0900

    Add texture file and Add Webpack Loader webpack Loader: mtl, obj files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const loader = new THREE.OBJLoader2();
 var webpack = require('webpack');
 
 const port = process.env.PORT || 5000;
